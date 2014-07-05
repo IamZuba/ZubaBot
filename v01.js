5ea0929017aa093cf9a7e21bc4353377d89b0b5c
IamZuba <alecjensenpugh@live.com>
2014-07-05 02:05:55 -0400

    Update v01.js

--- v01.js
+++ v01.js
@@ -2507,7 +2507,7 @@ var esBot = {
                                 if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                                 if( !esBot.commands.executable(this.rank, chat) ) return void (0);
                                 else{
-                                    API.sendChat('/me Please only meh songs if you truely think they are bad, not because you feel like it or you don't like the DJ.')
+                                    API.sendChat('/me Please only')
                                 };                              
                         },
                 },
