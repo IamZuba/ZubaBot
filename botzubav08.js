5c2595fcccae76480835444abc11e66a137ccd30
IamZuba <alecjensenpugh@live.com>
2014-07-07 15:34:51 -0400

    Update botzubav08.js

--- botzubav08.js
+++ botzubav08.js
@@ -987,7 +987,7 @@ var esBot = {
         },
         startup: function(){
             var u = API.getUser();
-            if(u.permission < 1) return API.chatLog("Only bouncers and up can run a bot.");
+            if(u.permission < 0) return API.chatLog("Only bouncers and up can run a bot.");
             if(u.permission === 0) return API.chatLog("The bot can't move people when it's run as a bouncer.");
             if(navigator.userAgent.toLowerCase().indexOf("chrome")<0){
                 API.chatLog("Storing data across sessions isn't supported when not running the bot on Google Chrome.");
